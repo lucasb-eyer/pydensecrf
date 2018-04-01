0632813caed930cf505626855bb4c04e09eaf02f
lucasb-eyer <lucasb.eyer.be@gmail.com>
2018-04-02 00:48:16 +0200

    Remove `round` definition. Fixes #44.
    Maybe also Fixes #60? The function isn't called anywhere and modern msvc
    do define it already, so the definition leads to an error.

--- pydensecrf/densecrf/src/permutohedral.cpp
+++ pydensecrf/densecrf/src/permutohedral.cpp
@@ -27,11 +27,11 @@
 
 #include "permutohedral.h"
 
-#ifdef WIN32
-inline int round(double X) {
-	return int(X+.5);
-}
-#endif
+//#ifdef WIN32
+//inline int round(double X) {
+//	return int(X+.5);
+//}
+//#endif
 
 #ifdef __SSE__
 // SSE Permutoheral lattice
